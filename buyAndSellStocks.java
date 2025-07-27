5ee51962f263287b9a6cc6b8b21d5aaa0f1dfe76
Viren Jawadwar <virenjawadwar001@gmail.com>
2025-07-27 16:09:40 +0530

    added comments

--- buyAndSellStocks.java
+++ buyAndSellStocks.java
@@ -1,36 +1,53 @@
 import java.util.*;
 
 class buyAndSellStocks {
+
+    // Method to calculate the maximum profit from buying and selling stocks
     public int maxProfit(int[] prices) {
-        int buyPrice=Integer.MAX_VALUE;
-        int maxp=0;
+        // Initialize buyPrice to a very large value so that any price will be lower initially
+        int buyPrice = Integer.MAX_VALUE;
+        
+        // Variable to keep track of the maximum profit
+        int maxp = 0;
 
-        for(int i=0;i<prices.length;i++) {
-            if(buyPrice<prices[i]) {
-                int profit=prices[i]-buyPrice;
+        // Iterate through the array of prices
+        for (int i = 0; i < prices.length; i++) {
+            // If current price is greater than the buyPrice, a profit can be made
+            if (buyPrice < prices[i]) {
+                // Calculate the profit for this transaction
+                int profit = prices[i] - buyPrice;
 
-                maxp=Math.max(maxp, profit);
+                // Update maxp if this profit is greater than previous maxp
+                maxp = Math.max(maxp, profit);
             } else {
-                buyPrice=prices[i];
+                // If current price is less than or equal to buyPrice,
+                // update buyPrice to this new lower price
+                buyPrice = prices[i];
             }
         }
+
+        // Return the maximum profit found
         return maxp;
     }
-    
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
+        // Read the number of days (size of the prices array)
+        int n = sc.nextInt();
 
-    int[] prices=new int[n];
+        // Initialize the prices array
+        int[] prices = new int[n];
 
-    for(int i=0;i<n;i++) {
-        prices[i]=sc.nextInt();
+        // Take input of prices for each day
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        // Create an object of the class to call the non-static method
+        buyAndSellStocks sol = new buyAndSellStocks();
+
+        // Print the result returned by maxProfit method
+        System.out.println(sol.maxProfit(prices));
     }
-
-    buyAndSellStocks sol=new buyAndSellStocks();
-    System.out.println(sol.maxProfit(prices));
-    
-}
 }
