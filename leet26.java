64a69133068bcdf08153981a9bd8bbf4ad943877
Viren Jawadwar <virenjawadwar001@gmail.com>
2025-08-28 17:50:28 +0530

    added leetcode 26 question

---
+++ leet26.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class leet26 {
+    public int removeDuplicates(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++) {
+            if(i<nums.length-1 && nums[i]==nums[i+1]) {
+                continue;
+            }
+            else {
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
